6f91f67bb647cef2669865646a026097a270c4b2
Clintonkipchirchir <clintonkipkemoi2@gmail.com>
2025-02-16 23:25:55 +0300

    add list and search bar

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pulication_year')
+    search_fields = ('title', 'author', 'pulication_year')
+
+admin.site.register(Book, BookAdmin)

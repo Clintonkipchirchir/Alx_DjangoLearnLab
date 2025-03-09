3bcb3e83dbc178476a2da36226b4d24ac8f2b76d
clinton <clintonkipkemoi2@gmail.com>
2025-03-09 15:36:42 +0300

    updated  auth and perms to api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
@@ -10,5 +11,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)), # Maps to the BookViewSet view
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), # Maps to the obtain_auth_token view
 ]
 

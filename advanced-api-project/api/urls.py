d7e8a34a40f5abf9ee32696753d40e721dc3440a
clinton <clintonkipkemoi2@gmail.com>
2025-03-16 23:02:10 +0300

    add crud capabilities to api app

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,6 +4,8 @@ from .views import BookDeleteView, BookUpdateView
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-get'),
-    path('books/add/', BookCreateView.as_view(), name='add-book'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='get-book'),
+    path('books/create/', BookCreateView.as_view(), name='create-book'),
+    path('books/delete/', BookDeleteView.as_view(), name='delete-book'),
+    path('books/update/', BookUpdateView.as_view(), name='update-book'),
 ]

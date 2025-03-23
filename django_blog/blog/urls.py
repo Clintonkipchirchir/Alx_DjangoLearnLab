819c97fc96419b4d9c92000addfcf7d8321f556e
clinton <clintonkipkemoi2@gmail.com>
2025-03-23 21:16:04 +0300

    added crud ops

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="blog/login.html", authentication_form=LoginForm), name='login'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 
 ]

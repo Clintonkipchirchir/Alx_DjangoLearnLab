10bd04eaa400a5ce2601bf5de42ab551fd322be3
clinton <clintonkipkemoi2@gmail.com>
2025-03-23 20:25:23 +0300

    added login form

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Post
+from .forms import RegisterForm, LoginForm
+
+def profile(request):
+    posts = Post.object.all()
+    pass
+
+    
+
+def registration(request):
+     if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+     else:
+        form = RegisterForm()
+
+     return render(request, 'blog/registration.html', {
+        'form': form
+    })
+
+def profile*()

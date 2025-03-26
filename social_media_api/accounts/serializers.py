3ccb44a889f7e61c42f8251ef2b3d4088f9ebce6
clinton <clintonkipkemoi2@gmail.com>
2025-03-27 01:17:40 +0300

    add user creation logic in UserSerializer to handle password hashing

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,3 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'password']
+
+        def create(self, validate_data):
+            username = validate_data["username"]           
+            email = validate_data["email"]   
+            password = validate_data["password"]   
+
+            user = get_user_model()
+            new_user = user.objects.create_user(email=email, username=username)
+            new_user.set_password(password)
+            new_user.save()
+            return new_user

d80faa8db8731d5e46d61bb640a7b50f06340b60
clinton <clintonkipkemoi2@gmail.com>
2025-04-07 01:28:51 +0300

    updated file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -123,8 +123,8 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        like, created = Like.objects.get_or_create(post=post, user=request.user)
+        post = generics.get_object_or_404(Post, pk=pk)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
         if not created:
             return Response({'detail': 'Already liked'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -142,9 +142,9 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         try:
-            like = Like.objects.get(post=post, user=request.user)
+            like = Like.objects.get(user=request.user, post=post)
             like.delete()
             return Response({'detail': 'Post unliked'}, status=status.HTTP_200_OK)
         except Like.DoesNotExist:
